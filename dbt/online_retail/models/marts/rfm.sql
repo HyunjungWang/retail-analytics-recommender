4e3b7206785927adacf82e814de37e8212343dcf
HyunjungWang <imwhj1997@gmail.com>
2025-05-08 01:30:07 +0200

    add RPR and year in trend

--- dbt/online_retail/models/marts/rfm.sql
+++ dbt/online_retail/models/marts/rfm.sql
@@ -1,11 +1,26 @@
-WITH rfm AS (
+WITH max_date AS (
+  SELECT MAX(DATE(InvoiceDate)) AS latest_date
+  FROM {{ ref('stg_sales') }}
+),
+rfm AS (
   SELECT 
     customer_id,
-    DATE_DIFF(CURRENT_DATE(), DATE(MAX(InvoiceDate)), DAY) AS recency,  -- Convert MAX(InvoiceDate) to DATE
+    DATE_DIFF((SELECT latest_date FROM max_date), DATE(MAX(InvoiceDate)), DAY) AS recency,
     COUNT(DISTINCT Invoice) AS frequency,
     SUM(TotalPrice) AS monetary
-  FROM {{ ref('stg_sales') }}  -- Reference your staging table
+  FROM {{ ref('stg_sales') }}
   GROUP BY customer_id
 )
+,
 
-SELECT * FROM rfm
+scored_rfm AS (
+  SELECT *,
+    NTILE(5) OVER (ORDER BY recency DESC) AS recency_score,  -- Lower recency = higher score
+    NTILE(5) OVER (ORDER BY frequency ASC) AS frequency_score,
+    NTILE(5) OVER (ORDER BY monetary ASC) AS monetary_score
+  FROM rfm
+)
+
+SELECT *,
+       CONCAT(recency_score, frequency_score, monetary_score) AS rfm_segment
+FROM scored_rfm
